f29182e2375647062902e55b9edc1e8cab60e361
rajaadil07 <69093234+rajaadil07@users.noreply.github.com>
2022-01-16 15:07:37 -0800

    Modified Pet Notes

--- PetNotes.js
+++ PetNotes.js
@@ -1,29 +1,44 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, SafeAreaView, Image, ScrollView, TouchableOpacity, TextInput } from "react-native";
+import { StyleSheet, Text, View, SafeAreaView, TextInput } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
+
 
 export function PetNotes() {
-  const [note,setNote] = useState("None");
+  const [note,setNote] = useState("");
+  const navigation = useNavigation();
 
   return (
 
     <SafeAreaView style={styles.container}>
-    <View style={styles.textAreaContainer} >
-      <Text style={[styles.text, { fontWeight: "200", fontSize: 36, color: 'blue'}]}>Add a Note</Text>
-      <TextInput
-        style={styles.textArea}
-        underlineColorAndroid="transparent"
-        placeholder=""
-        placeholderTextColor="grey"
-        numberOfLines={10}
-        multiline={true}
-        onChangeText={(val) => setNote(val)}
-      />
+        <View style={styles.titleBar}>
+            <Ionicons
+              name="ios-arrow-back"
+              size={24}
+              color="#3153A4"
+              onPress={() => navigation.navigate("PetList")}
+            ></Ionicons>
+            <Ionicons name="add" size={24} color="#3153A4"></Ionicons>
+        </View>
+
+      <View style={styles.textAreaContainer} >
+        <Text style={[styles.text, { fontWeight: "200", fontSize: 36, color: 'blue'}]}>Add a Note</Text>
+        <TextInput
+          style={styles.textArea}
+          underlineColorAndroid="transparent"
+          placeholder=""
+          placeholderTextColor="grey"
+          numberOfLines={10}
+          multiline={true}
+          onChangeText={(val) => setNote(val)}
+        />
       </View>
+    
       <View style={styles.infoContainer}>
-        <Text style={[styles.text, { fontWeight: "200", fontSize: 35, color: 'blue'}]}>Hazel's Notes</Text>
-        <Text style={[styles.text, { fontWeight: "200", fontSize: 25, color: 'blue'}]}>{note}</Text>
+          <Text style={[styles.text, { fontWeight: "200", fontSize: 35, color: 'blue'}]}>Hazel's Notes</Text>
+          <Text style={[styles.text, { fontWeight: "200", fontSize: 25, color: 'blue'}]}>{note}</Text>
       </View>
-      </SafeAreaView>
+    </SafeAreaView>
       
       
   )
